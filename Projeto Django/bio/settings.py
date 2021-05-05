6a1056f6bfbb44f442cf87630d699fc0225bf020
victorsantosok <42875522+victorsantosok@users.noreply.github.com>
2021-05-05 12:01:25 -0300

    Dark-theme

--- Projeto Django/bio/settings.py
+++ Projeto Django/bio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*x64-lf9*^o#&vyw&91drn!p1$e!rvw_oz06z4xls%qjrwq1h)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.142.255.93']
+ALLOWED_HOSTS = ['3.142.255.93','127.0.0.1']
 
 
 # Application definition
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'bio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'paginas')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'templates/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
