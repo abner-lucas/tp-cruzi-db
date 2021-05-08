6687038748deacd91a878ed865e20da5f79bd113
Ábner Lucas <abnerlucas.cad@gmail.com>
2021-05-08 07:12:12 -0300

    finalização da consulta do Blast

--- Projeto Django/blast/views.py
+++ Projeto Django/blast/views.py
@@ -3,19 +3,35 @@ from .out import Blast
 from .forms import UploadFileForm
 from .models import meuarquivo
 from django.http import HttpResponseRedirect
+import os
+from . import views
 
 def index(request):
     return render(request, 'blast/index.html')
 
-def GetResponse(request):
-    sequencia = request.POST.get('seqtxtBlast')
-    response = Blast.execute_blast(sequencia, "blastn", "genomic")
-    print(sequencia)
+def GetResponse(request): 
+    
+    if request.POST.get('seqtxtBlast', False):       # SE seqtxtBlast ESTIVER SETADO
+        arquivo_fasta = open('blast\\out\\query_blast.fasta', 'w+')
+        arquivo_fasta.writelines(request.POST.get('seqtxtBlast'))
+        arquivo_fasta.close()
+    
+    elif request.FILES['seqfileBlast']:       #SE seqfileBlast ESTIVER SETADO
+        arquivo = request.FILES['seqfileBlast']
+        fn = 'blast\\out\\' + os.path.basename(arquivo.name)
+        open(fn, 'wb').write(arquivo.file.read())
+        
+        arquivo_enviado = UploadFileForm(request.POST, request.FILES)
+        arquivo_enviado = request.FILES['seqfileBlast']
+        upload_path = 'blast/out/' + arquivo_enviado.name
+    
+        #RENOMEADO O ARQUIVO CARREGADO
+        os.rename(r'blast/out/' + arquivo_enviado.name, r'blast/out/query_blast.fasta')
+
+    response = Blast.execute_blast("blast\\out\\query_blast.fasta", "blastn", "genomic")
+    #response = Blast.execute_blast('blast\\out\\exemplo.fasta', 'blastn', 'genomic')
+    
     context={
       'object': response
     }
     return render(request,"blast/responseblast.html", context)
-
-def upload_file(request):
-    form = UploadFileForm(request.POST, request.FILES)
-    return render(request, 'upload.html', {'form': form})   
