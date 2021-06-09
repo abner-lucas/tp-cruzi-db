cf686269ed990e71f039d28691e4a7070e90850a
mayanapontes <mayanacpontes@gmail.com>
2021-06-08 23:43:02 -0300

    Adição do menu do Jbrowse Remoção do nome do Márcio

--- WebProject/blast/out/Blast.py
+++ WebProject/blast/out/Blast.py
@@ -20,8 +20,7 @@ def execute_blast(filename, blast, subject):
 	comando_blastn = NcbiblastnCommandline(cmd=path+blast, \
 										   query=filename, subject='blast/out/GCF_000209065.1_ASM20906v1_' + files.get(subject), \
 										   outfmt=5, out='blast/out/result_blast.xml')
-	#print(comando_blastn)
-	comando_blastn()
+
 	
 	arquivo_xml = open('blast/out/result_blast.xml', "r")
 	dados = NCBIXML.parse(arquivo_xml)
@@ -35,26 +34,30 @@ def execute_blast(filename, blast, subject):
 		for hsp in a.hsps:
 			alinhamento = {}
 			alinhamento['num'] = i
-			alinhamento['sequencia'] = a.title
-			alinhamento['tamanho'] = a.length
-			alinhamento['score'] = hsp.score
-			alinhamento['bits'] = hsp.bits
-			alinhamento['expect'] = f"{float(hsp.expect):.2e}"
-			alinhamento['num_alignments'] = hsp.num_alignments 
-			alinhamento['identities'] = hsp.identities 
-			alinhamento['positives'] = hsp.positives
-			alinhamento['gaps'] = hsp.gaps
-			alinhamento['align_length'] = hsp.align_length
-			alinhamento['strand'] = hsp.strand
-			alinhamento['frame '] = hsp.frame
-			alinhamento['query'] = hsp.query
-			alinhamento['query_start'] = hsp.query_start 
-			alinhamento['query_end'] = hsp.query_end
-			alinhamento['match'] = hsp.match
-			alinhamento['subject'] = hsp.sbjct
-			alinhamento['subject_start'] = hsp.sbjct_start
-			alinhamento['subject_end'] = hsp.sbjct_end
+			alinhamento['sequencia'] = getProperty(a.title)
+			alinhamento['tamanho'] = getProperty(a.length)
+			alinhamento['score'] = getProperty(hsp.score)
+			alinhamento['bits'] = getProperty(hsp.bits)
+			alinhamento['expect'] = getProperty(f"{float(hsp.expect):.2e}")
+			alinhamento['num_alignments'] = getProperty(hsp.num_alignments)
+			alinhamento['identities'] = getProperty(hsp.identities)
+			alinhamento['positives'] = getProperty(hsp.positives)
+			alinhamento['gaps'] = getProperty(hsp.gaps)
+			alinhamento['align_length'] = getProperty(hsp.align_length)
+			alinhamento['strand'] = getProperty(hsp.strand)
+			alinhamento['frame'] = getProperty(hsp.frame)
+			alinhamento['query'] = getProperty(hsp.query)
+			alinhamento['query_start'] = getProperty(hsp.query_start)
+			alinhamento['query_end'] = getProperty(hsp.query_end)
+			alinhamento['match'] = getProperty(hsp.match)
+			alinhamento['subject'] = getProperty(hsp.sbjct)
+			alinhamento['subject_start'] = getProperty(hsp.sbjct_start)
+			alinhamento['subject_end'] = getProperty(hsp.sbjct_end)
 			itens.append(alinhamento)
 			i+=1
 
 	return itens
+
+
+def getProperty(property):
+	return str(property).replace("None", "").replace("True", "true").replace("False", "");
